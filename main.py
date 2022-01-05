12d7a376f18bdfe20f964cc33d88924683c77db2
overvalue <97093342+overvalue@users.noreply.github.com>
2022-01-05 17:08:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyautogui as pg
+import time
+import random
+
+input("Press Enter to Start ")
+time.sleep(3)
+print("-----")
+while True:
+	time.sleep(random.randint(25, 30))
+	pg.typewrite('+work\n',0.5)
+	time.sleep(1)
+	time.sleep(random.randint(25, 30))
+	pg.typewrite('+work\n',0.5)
+	time.sleep(1)
+	time.sleep(random.randint(25, 30))
+	pg.typewrite('+work\n',0.5)
+	time.sleep(7)
+	pg.typewrite('+dep all\n',0.2)
+
+
+
+	time.sleep(random.randint(20, 42))
+
+
+
+
